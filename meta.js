976039cd63ae9462aba8d9f6a7af069b6ba9ac34
杨谱 <yangpu@MacBook-Pro.local>
2020-10-10 12:20:55 +0800

    first commit

---
+++ meta.js
@@ -0,0 +1,59 @@
+const path = require('path')
+
+// const {
+//   sortDependencies,
+//   installDependencies,
+//   runLintFix,
+//   printMessage,
+// } = require('./utils')
+const pkg = require('./package.json')
+
+// const templateVersion = pkg.version
+
+// const { addTestAnswers } = require('./scenarios')
+
+module.exports = {
+  prompts: {
+    name: {
+      when: 'isNotTest',
+      type: 'string',
+      required: true,
+      message: '请输入项目名称：',
+    },
+    description: {
+      when: 'isNotTest',
+      type: 'string',
+      required: false,
+      message: '请输入项目描述：',
+      default: 'VUE项目',
+    },
+    author: {
+      when: 'isNotTest',
+      type: 'string',
+      message: '请输入作者名字：',
+    }
+  },
+  complete: function(data, { chalk }) {
+    console.log('complate')
+    // const green = chalk.green
+
+    // sortDependencies(data, green)
+
+    // const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+    // if (data.autoInstall) {
+    //   installDependencies(cwd, data.autoInstall, green)
+    //     .then(() => {
+    //       return runLintFix(cwd, data, green)
+    //     })
+    //     .then(() => {
+    //       printMessage(data, green)
+    //     })
+    //     .catch(e => {
+    //       console.log(chalk.red('Error:'), e)
+    //     })
+    // } else {
+    //   printMessage(data, chalk)
+    // }
+  },
+}
