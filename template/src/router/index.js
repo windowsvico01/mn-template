9d5819c11157e3613d131f912c33a4315d541f1f
杨谱 <yangpu@MacBook-Pro.local>
2020-10-12 12:08:17 +0800

    update

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/home/index.vue'
 
-import localStorage from '@/utils/localStorage'
+// import localStorage from '@/utils/localStorage'
 
 Vue.use(VueRouter)
 
@@ -25,7 +25,7 @@ const router = new VueRouter({
 
 
 router.beforeEach((to, from, next) => {
-  const token = localStorage.get('token');
+  // const token = localStorage.get('token');
   // if (to.query.SharePeople) {
   //   localStorage.set('SharePeople', to.query.SharePeople)
   // }
